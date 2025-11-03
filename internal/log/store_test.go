030ad26a6132b8a0644ce0501aecbbb09dce2c3c
Nicolas <admin@MacBook-Pro.local>
2025-11-03 20:19:54 +0300

    test close method

--- internal/log/store_test.go
+++ internal/log/store_test.go
@@ -12,8 +12,9 @@ var (
 	width = uint64(len(write)) + lenWidth
 )
 
+// TestStoreAppenRead tests Append and Read method
 func TestStoreAppenRead(t *testing.T) {
-	f, err := os.CreateTemp("", "store_append_read_test")
+	f, err := os.CreateTemp("", "store")
 	require.NoError(t, err)
 	defer os.Remove(f.Name())
 
@@ -21,7 +22,6 @@ func TestStoreAppenRead(t *testing.T) {
 	require.NoError(t, err)
 
 	testAppend(t, s)
-
 	testRead(t, s)
 	testReatAt(t, s)
 
@@ -31,6 +31,44 @@ func TestStoreAppenRead(t *testing.T) {
 	testRead(t, s)
 }
 
+// TestStoreClose tests close store method
+func TestStoreClose(t *testing.T) {
+	f, err := os.CreateTemp("", "store")
+	require.NoError(t, err)
+
+	s, err := NewStore(f)
+	require.NoError(t, err)
+
+	_, _, err = s.Append([]byte(write))
+	require.NoError(t, err)
+
+	_, beforeSize, err := openFile(f.Name())
+	require.NoError(t, err)
+
+	err = s.Close()
+	require.NoError(t, err)
+
+	_, afterSize, err := openFile(f.Name())
+	require.NoError(t, err)
+
+	require.True(t, afterSize > beforeSize)
+}
+
+// openFile represents a helper function to open file
+func openFile(name string) (file *os.File, size int64, err error) {
+	f, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, 0, err
+	}
+	return f, fi.Size(), nil
+}
+
+// testReatAt tests the ReatAt method
 func testReatAt(t *testing.T, s *store) {
 	t.Helper()
 
@@ -52,6 +90,7 @@ func testReatAt(t *testing.T, s *store) {
 
 }
 
+// testAppend represents a helper function to test Append
 func testAppend(t *testing.T, s *store) {
 	t.Helper()
 	for i := uint64(1); i < 4; i++ {
@@ -61,6 +100,7 @@ func testAppend(t *testing.T, s *store) {
 	}
 }
 
+// testRead represents a helper function to test Read
 func testRead(t *testing.T, s *store) {
 	t.Helper()
 	var pos uint64
