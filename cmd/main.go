f3b289f2f1315acc0e162bfa3505e995db65fb94
Nicolas <admin@MacBook-Pro.local>
2025-11-16 20:54:31 +0300

    add read method for segment

--- cmd/main.go
+++ cmd/main.go
@@ -2,61 +2,31 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
 
-	"github.com/nico-phil/go-log/internal/log"
+	api "github.com/nico-phil/go-log/api/v1"
+	llog "github.com/nico-phil/go-log/internal/log"
 )
 
 func main() {
-	f, err := os.Create("storelog")
-	if err != nil {
-		return
-	}
-
-	s, _ := log.NewStore(f)
-
-	n, pos, err := s.Append([]byte("first"))
-	if err != nil {
-		return
-	}
-
-	fmt.Printf("n: %d\n", n)
-	fmt.Printf("pos: %d\n", pos)
-
-	d1, _ := s.Read(pos)
-	fmt.Println("d1", string(d1))
-
-	n, pos, err = s.Append([]byte("second"))
-	if err != nil {
-		return
-	}
-
-	fmt.Printf("n: %d\n", n)
-	fmt.Printf("pos: %d\n", pos)
-
-	d2, _ := s.Read(pos)
-	fmt.Println("d2", string(d2))
-	fmt.Println("------------------------------------")
-
-	fIn, err := os.Create("index_test")
-	if err != nil {
-		return
-	}
-	c := log.Config{}
+	c := llog.Config{}
 	c.Segment.MaxIndexBytes = 1024
-	newIndexFile, err := log.NewIndex(fIn, c)
+	sg, err := llog.NewSegment("data", 0, c)
 	if err != nil {
-		fmt.Println("newIndexFile:", err)
-		return
+
+		log.Fatal(err)
 	}
 
-	newIndexFile.Write(1, 10)
-	newIndexFile.Write(2, 6)
-	resBuf := make([]byte, 1024)
+	log.Printf("%+v", *sg)
+	r := api.Record{
+		Value: []byte("hello world"),
+	}
 
-	fmt.Println(newIndexFile.MMap)
+	off, err := sg.Append(&r)
+	if err != nil {
+		log.Fatal("append:", err)
+	}
 
-	nu, _ := f.Read(resBuf)
-	fmt.Println("resBuf", string(resBuf[0:nu]))
+	fmt.Println(off)
 
 }
