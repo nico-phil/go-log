67c6ef58d8dc285a26c6f97548cd3eb899d951ef
Nicolas <admin@MacBook-Pro.local>
2025-11-17 17:02:49 +0300

    continue testing

--- cmd/main.go
+++ cmd/main.go
@@ -5,26 +5,27 @@ import (
 	"log"
 
 	api "github.com/nico-phil/go-log/api/v1"
+
 	llog "github.com/nico-phil/go-log/internal/log"
 )
 
 func main() {
 	c := llog.Config{}
-	c.Segment.MaxIndexBytes = 1024
-	sg, err := llog.NewSegment("data", 0, c)
+	c.Segment.MaxIndexBytes = 12 * 3
+	c.Segment.MaxStoreBytes = 1024
+	sg, err := llog.NewSegment("data", 16, c)
 	if err != nil {
-
 		log.Fatal(err)
 	}
 
-	log.Printf("%+v", *sg)
+	fmt.Printf("%+v", *sg)
 	r := api.Record{
 		Value: []byte("hello world"),
 	}
 
 	off, err := sg.Append(&r)
 	if err != nil {
-		log.Fatal("append:", err)
+		log.Fatal("append(main):\n", err)
 	}
 
 	fmt.Println(off)
