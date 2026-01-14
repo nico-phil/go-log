4dd73ed70e2c86efaf9a920bea132a6b988eab8e
Nicolas <admin@MacBook-Pro.local>
2026-01-14 16:24:21 +0300

    add comments for docs

--- internal/config/files.go
+++ internal/config/files.go
@@ -6,25 +6,31 @@ import (
 )
 
 var (
+	// Certificate Autority file
 	CAFile = configFile("ca.pem")
 
+	// certificate and private key file for server
 	ServerCertFile = configFile("server.pem")
 	ServerKeyFile  = configFile("server-key.pem")
 
+	// certificate and private key file for client
 	ClientCertFile = configFile("client.pem")
 	ClientKeyFile  = configFile("client-key.pem")
 
+	// certificate and private key file for client
 	RootClientCertFile = configFile("root-client.pem")
 	RootClientKeyFile  = configFile("root-client-key.pem")
 
+	// / certificate and private key file for unthorize client(for testing purpose)
 	NoBodyClientCertFile = configFile("nobody-client.pem")
 	NoBodyClientKeyFile  = configFile("nobody-client-key.pem")
 
+	// Casbin model and policy file
 	ACLModelFile  = configFile("model.conf")
 	ACLPolicyFile = configFile("policy.csv")
 )
 
-// configFile returns path of certs file
+// configFile returns the full path of certs and other file
 func configFile(filename string) string {
 	if dir := os.Getenv("CONFIG_DIR"); dir != "" {
 		return filepath.Join(dir, filename)
