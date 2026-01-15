04af8a097495948d889c6abe46785e18f89232dd
Nicolas <admin@MacBook-Pro.local>
2026-01-15 21:50:04 +0300

    trying log with zap

--- internal/server/grpc_server.go
+++ internal/server/grpc_server.go
@@ -2,9 +2,17 @@ package server
 
 import (
 	"context"
+	"time"
 
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	"go.opencensus.io/plugin/ocgrpc"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/trace"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
 	api "github.com/nico-phil/go-log/api/v1"
 	llog "github.com/nico-phil/go-log/internal/log"
 	"google.golang.org/grpc"
@@ -54,14 +62,36 @@ func newgrpcServer(config *Config) (*grpcServer, error) {
 
 // NewGRPCServer creates a Newserver and and register it
 func NewGRPCServer(config *Config, opts ...grpc.ServerOption) (*grpc.Server, error) {
+
+	logger := zap.L().Named("server")
+	zapOpts := []grpc_zap.Option{
+		grpc_zap.WithDurationField(
+			func(duration time.Duration) zapcore.Field {
+				return zap.Int64(
+					"grpc.time_ns",
+					duration.Nanoseconds(),
+				)
+			},
+		),
+	}
+
+	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
+	err := view.Register(ocgrpc.DefaultServerViews...)
+	if err != nil {
+		return nil, err
+	}
+
 	opts = append(opts,
 		grpc.StreamInterceptor(
 			grpc_middleware.ChainStreamServer(
 				grpc_auth.StreamServerInterceptor(authenticate),
+				grpc_zap.StreamServerInterceptor(logger, zapOpts...),
 			)),
 		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
 			grpc_auth.UnaryServerInterceptor(authenticate),
+			grpc_zap.UnaryServerInterceptor(logger, zapOpts...),
 		)),
+		grpc.StatsHandler(&ocgrpc.ServerHandler{}),
 	)
 
 	gsrv := grpc.NewServer(opts...)
