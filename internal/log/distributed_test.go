13a506cc383a4bc64c3e1c4d899350ab4e9b3229
Nicolas <admin@MacBook-Pro.local>
2026-02-01 20:23:03 +0300

    testing distributed log

--- internal/log/distributed_test.go
+++ internal/log/distributed_test.go
@@ -1,7 +1,21 @@
 package log
 
-import "testing"
+import (
+	"os"
+	"testing"
 
+	"github.com/stretchr/testify/require"
+)
+
+// TestDistributedLog test the distributedLog
 func TestDistributedLog(t *testing.T) {
+	tempDir, err := os.MkdirTemp("", "data-log")
+	require.NoError(t, err)
+	defer os.Remove(tempDir)
+
+	config := Config{}
+	config.Segment.MaxIndexBytes = 1024
+	_, err = NewDistrubutedLog(tempDir, config)
+	require.NoError(t, err)
 
 }
