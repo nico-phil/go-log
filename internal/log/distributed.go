db6d80299240f5042a8bf92d3ae02d13806c79f5
Nicolas <admin@MacBook-Pro-2.local>
2026-02-19 14:04:12 +0300

    add streamlayer to implment raft streamlayer interface

--- internal/log/distributed.go
+++ internal/log/distributed.go
@@ -2,7 +2,10 @@ package log
 
 import (
 	"bytes"
+	"crypto/tls"
+	"fmt"
 	"io"
+	"net"
 	"os"
 	"path"
 	"path/filepath"
@@ -20,10 +23,6 @@ type DistributedLog struct {
 	config Config
 	log    *Log
 	raft   *raft.Raft
-}
-
-type logStore struct {
-	*Log
 }
 
 // NewDistrubutedLog create a new distributed log
@@ -146,7 +145,7 @@ func (l *DistributedLog) setupRaft(dataDir string) error {
 	return err
 }
 
-// Append appends a record to the wal log
+// Append appends a record to the log
 func (l *DistributedLog) Append(record *api.Record) (uint64, error) {
 	res, err := l.apply(
 		AppendRequestType,
@@ -198,44 +197,19 @@ func (l *DistributedLog) Read(offset uint64) (*api.Record, error) {
 	return l.log.Read(offset)
 }
 
-func (l *logStore) GetLog(index uint64, out *raft.Log) error {
-	in, err := l.Read(index)
-	if err != nil {
-		return err
-	}
-	out.Data = in.Value
-	out.Index = in.Offset
-	out.Type = raft.LogType(in.Type)
-	out.Term = in.Term
-	return nil
-}
-
-func newLogStore(logDir string, config Config) (*logStore, error) {
-	return nil, nil
-}
-
-func (l *logStore) DeleteRange(min, max uint64) error {
-	return l.Truncate(max)
-}
-
-func (l *logStore) FirstIndex() (uint64, error) {
-	return l.LowestOffset()
-}
-
-func (l *logStore) LastIndex() (uint64, error) {
-	off, err := l.HighestOffset()
-	return off, err
-}
-
 func (l *DistributedLog) Create() error {
 	return nil
 }
 
 var _ raft.FSM = (*fsm)(nil)
 
+// fsm represents the finite-state machine
 type fsm struct {
 	log *Log
 }
+
+// RequestType represents the request type: in this case we have only APPEND.
+// We could also have DELETE, UPDATE ect..
 type RequestType uint8
 
 const (
@@ -252,6 +226,7 @@ func (l *fsm) Apply(record *raft.Log) interface{} {
 	return nil
 }
 
+// applyAppend actually applies the command stored in the finite-state machine to wal
 func (l *fsm) applyAppend(b []byte) interface{} {
 	var req api.ProduceRequest
 	err := proto.Unmarshal(b, &req)
@@ -264,6 +239,27 @@ func (l *fsm) applyAppend(b []byte) interface{} {
 	}
 	return &api.ProduceResponse{Offset: int64(offset)}
 }
+
+func (f *fsm) Snapshot() (raft.FSMSnapshot, error) {
+	r := f.log.Reader()
+	return &snapshot{reader: r}, nil
+}
+
+var _ raft.FSMSnapshot = (*snapshot)(nil)
+
+type snapshot struct {
+	reader io.Reader
+}
+
+func (s *snapshot) Persist(sink raft.SnapshotSink) error {
+	if _, err := io.Copy(sink, s.reader); err != nil {
+		_ = sink.Cancel()
+		return err
+	}
+	return sink.Close()
+}
+
+func (s *snapshot) Release() {}
 
 func (f *fsm) Restore(r io.ReadCloser) error {
 	b := make([]byte, lenWidth)
@@ -297,28 +293,45 @@ func (f *fsm) Restore(r io.ReadCloser) error {
 	return nil
 }
 
-func (f *fsm) Snapshot() (raft.FSMSnapshot, error) {
-	r := f.log.Reader()
-	return &snapshot{reader: r}, nil
+var _ raft.LogStore = (*logStore)(nil)
+
+type logStore struct {
+	*Log
 }
 
-var _ raft.FSMSnapshot = (*snapshot)(nil)
-
-type snapshot struct {
-	reader io.Reader
+func newLogStore(dir string, config Config) (*logStore, error) {
+	log, err := NewLog(dir, config)
+	if err != nil {
+		return nil, err
+	}
+	return &logStore{Log: log}, nil
 }
 
-func (s *snapshot) Persist(sink raft.SnapshotSink) error {
-	if _, err := io.Copy(sink, s.reader); err != nil {
-		_ = sink.Cancel()
+func (l *logStore) FirstIndex() (uint64, error) {
+	return l.LowestOffset()
+}
+
+func (l *logStore) LastIndex() (uint64, error) {
+	off, err := l.HighestOffset()
+	return off, err
+}
+
+func (l *logStore) GetLog(index uint64, out *raft.Log) error {
+	in, err := l.Read(index)
+	if err != nil {
 		return err
 	}
-	return sink.Close()
+	out.Data = in.Value
+	out.Index = in.Offset
+	out.Type = raft.LogType(in.Type)
+	out.Term = in.Term
+	return nil
 }
 
 func (l *logStore) StoreLog(record *raft.Log) error {
 	return l.StoreLogs([]*raft.Log{record})
 }
+
 func (l *logStore) StoreLogs(records []*raft.Log) error {
 	for _, record := range records {
 		if _, err := l.Append(&api.Record{
@@ -330,4 +343,83 @@ func (l *logStore) StoreLogs(records []*raft.Log) error {
 		}
 	}
 	return nil
+}
+
+func (l *logStore) DeleteRange(min, max uint64) error {
+	return l.Truncate(max)
+}
+
+var _ raft.StreamLayer = (*StreamLayer)(nil)
+
+// StreamLayer represent a streamlayer. it should implment the raft.Streamlayer interface
+type StreamLayer struct {
+	ln              net.Listener
+	serverTLSConfig *tls.Config
+	peerTLSConfig   *tls.Config
+}
+
+func NewStreamLayer(
+	ln net.Listener,
+	serverTlsConfig *tls.Config,
+	peerTlsConfig *tls.Config,
+) *StreamLayer {
+	return &StreamLayer{
+		ln:              ln,
+		serverTLSConfig: serverTlsConfig,
+		peerTLSConfig:   peerTlsConfig,
+	}
+}
+
+const RaftRPC = 1
+
+func (s *StreamLayer) Dial(
+	addr raft.ServerAddress,
+	timeout time.Duration,
+) (net.Conn, error) {
+	dialer := &net.Dialer{Timeout: timeout}
+	var conn, err = dialer.Dial("tcp", string(addr))
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = conn.Write([]byte{byte(RaftRPC)})
+	if err != nil {
+		return nil, err
+	}
+
+	if s.peerTLSConfig != nil {
+		conn = tls.Client(conn, s.peerTLSConfig)
+	}
+
+	return conn, err
+}
+
+func (s *StreamLayer) Accept() (net.Conn, error) {
+	conn, err := s.ln.Accept()
+	if err != nil {
+		return nil, err
+	}
+
+	b := make([]byte, 1)
+	_, err = conn.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	if bytes.Compare([]byte{byte(RaftRPC)}, b) != 0 {
+		return nil, fmt.Errorf("not a raft rpc")
+	}
+
+	if s.serverTLSConfig != nil {
+		return tls.Server(conn, s.serverTLSConfig), nil
+	}
+
+	return conn, nil
+}
+
+func (s *StreamLayer) Close() error {
+	return s.ln.Close()
+}
+
+func (s *StreamLayer) Addr() net.Addr {
+	return s.ln.Addr()
 }
