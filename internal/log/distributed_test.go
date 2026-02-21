ec7b16d6737e10ad33dae666829bdc17d23a1ab8
Nicolas <admin@MacBook-Pro-2.local>
2026-02-21 21:26:23 +0300

    create 3 nodes for testing

--- internal/log/distributed_test.go
+++ internal/log/distributed_test.go
@@ -1,21 +1,48 @@
 package log_test
 
 import (
+	"fmt"
+	"net"
 	"os"
 	"testing"
+	"time"
 
+	"github.com/hashicorp/raft"
+	"github.com/nico-phil/go-log/internal/log"
 	"github.com/stretchr/testify/require"
+	"github.com/travisjeffery/go-dynaport"
 )
 
 // TestDistributedLog test the distributedLog
 func TestDistributedLog(t *testing.T) {
+	dataDir := ""
 	nodeCount := 3
+	ports := dynaport.Get(nodeCount)
 	for i := 0; i < nodeCount; i++ {
 		dataDir, err := os.MkdirTemp("", "distributed-log-test")
 		require.NoError(t, err)
 		defer func(dir string) {
 			_ = os.Remove(dir)
 		}(dataDir)
+
+		ln, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", ports[i]))
+		require.NoError(t, err)
+
+		config := log.Config{}
+
+		config.Raft.StreamLayer = log.NewStreamLayer(ln, nil, nil)
+		config.Raft.LocalID = raft.ServerID(fmt.Sprintf("%d", i))
+		config.Raft.HeartbeatTimeout = 50 * time.Millisecond
+		config.Raft.ElectionTimeout = 50 * time.Millisecond
+		config.Raft.LeaderLeaseTimeout = 50 * time.Millisecond
+		config.Raft.CommitTimeout = 5 * time.Millisecond
+
+		if i == 0 {
+			config.Raft.Bootstrap = true
+		}
+
+		_, err = log.NewDistrubutedLog(dataDir, config)
+		require.NoError(t, err)
 	}
 
 }
