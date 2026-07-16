b363ad63a3c69a6df1e9addcd2f4d667ee9d1789
Nicolas <admin@MacBook-Pro-3.local>
2026-07-16 22:21:35 +0300

    add consume stream cmd

---
+++ cmd/consume-stream/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	api "github.com/nico-phil/go-log/api/v1"
+	"github.com/nico-phil/go-log/internal/config"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+func main() {
+	addr := flag.String("addr", ":8400", "service addr")
+
+	peerTlsConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile: config.RootClientCertFile,
+		KeyFile:  config.RootClientKeyFile,
+		CAFile:   config.CAFile,
+		Server:   false,
+	})
+
+	tlsCreds := credentials.NewTLS(peerTlsConfig)
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(tlsCreds)}
+
+	conn, err := grpc.NewClient(*addr, opts...)
+	if err != nil {
+		fmt.Printf("Error creating client: %v\n", err)
+		return
+	}
+
+	client := api.NewLogClient(conn)
+
+	resp, err := client.ConsumeStream(context.Background(), &api.ConsumeRequest{Offset: 0})
+	if err != nil {
+		fmt.Printf("Error consuming record: %v\n", err)
+		return
+	}
+
+	for {
+		record, err := resp.Recv()
+		if err != nil {
+			fmt.Printf("Error receiving record: %v\n", err)
+			return
+		}
+		fmt.Printf("Response: %v\n", record)
+	}
+
+}
